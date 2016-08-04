6a20e93b54ecfa25f240ab0d8d047db1a88d4122
Udit Kapahi <ukitkk@gmail.com>
2016-08-04 15:20:09 +0530

    Doc controler and routes.

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,41 @@
+class DocsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_doc
+
+  end
+
+  def find_doc_params
+
+  end
+
+end

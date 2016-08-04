625158e979a9eb3b16ba17c7078ad4a49aecd7c0
Udit Kapahi <ukitkk@gmail.com>
2016-08-04 17:25:54 +0530

    Added some more features. Now you can edit update create or delete the docs,

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -16,22 +16,28 @@ class DocsController < ApplicationController
   def create
     @doc=Doc.create(doc_params)
     if @doc.save
-      redirect_to 'index'
+      redirect_to docs_path
     else
       render 'new'
     end
   end
 
   def edit
-
+    @doc=Doc.find(params[:id])
   end
 
   def update
-
+    if @doc.update(doc_params)
+      redirect_to  doc_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    if @doc.destroy
+      redirect_to docs_path
+    end
   end
 
   private
